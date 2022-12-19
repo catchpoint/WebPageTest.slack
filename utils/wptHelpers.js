560dd7413df25d0926c54fc75ac774fa0ab34641
Siddhant Shukla <70441430+Siddhantshukla814@users.noreply.github.com>
2022-12-19 13:13:07 +0530

    unnecessary console.log removal

--- utils/wptHelpers.js
+++ utils/wptHelpers.js
@@ -1,7 +1,9 @@
 exports.runTest = (wpt, url, options) => {
   const tempOptions = JSON.parse(JSON.stringify(options));
   return new Promise((resolve, reject) => {
-    console.info(`Submitting test for ${url}...`);
+    if (process.env.ENVIRONMENT === "DEVELOPMENT") {
+      console.info(`Submitting test for ${url}...`);
+    }
     wpt.runTest(url, tempOptions, async (err, result) => {
       try {
         if (result) {
@@ -19,7 +21,9 @@ exports.runTest = (wpt, url, options) => {
 exports.getLocations = (wpt, options) => {
   const tempOptions = JSON.parse(JSON.stringify(options));
   return new Promise((resolve, reject) => {
-    console.info(`Getting Locations...`);
+    if (process.env.ENVIRONMENT === "DEVELOPMENT") {
+      console.info(`Getting Locations...`);
+    }
     wpt.getLocations(tempOptions, async (err, result) => {
       try {
         if (result) {
